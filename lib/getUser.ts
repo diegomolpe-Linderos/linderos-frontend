c9687d49b032b9ce8116b7110d238c4be78245b3
diegomolpe-Linderos <diego.molpe@gmail.com>
2025-11-19 17:15:52 -0300

    fix: getUser.ts corregido

--- lib/getUser.ts
+++ lib/getUser.ts
@@ -1,1 +1,14 @@
-getSupabaseClient()
+import { getSupabaseClient } from "./supabase";
+
+export async function getUser() {
+  const supabase = getSupabaseClient();
+
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error) {
+    console.error("Error obteniendo el usuario:", error.message);
+    return null;
+  }
+
+  return data.user;
+}
